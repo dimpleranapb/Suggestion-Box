12aa6cb0735a5ba074f95d610151d4e48ea53867
Dimple Rana <dimpleranaji@gmail.com>
2024-12-04 10:37:39 +0530

    Setup email configurations

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,45 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import bcrypt from "bcrypt";
+
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  try {
+    const { username, email, password } = await request.json();
+    const existingUserVerifiedByUsername = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+    if (existingUserVerifiedByUsername) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username is already taken",
+        },
+        { status: 400 }
+      );
+    }
+
+    const existingUserByEmail = await UserModel.findOne({email})
+
+    if(existingUserByEmail){
+        true //TODO
+    }else{
+        
+    }
+  } catch (error) {
+    console.error("Error registering user", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error registering user",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,25 @@
+import {resend} from "@/lib/resend"
+import VerificationEmail from "../../emails/VerificationEmail"
+import  {ApiResponse} from '@/types/ApiResponse'
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    VerifyCode: string
+): Promise<ApiResponse> {
+    try{
+        await resend.emails.send({
+            from: 'Acme <onboarding@resend.dev>',
+            to: email,
+            subject: 'Verification Code',
+            react: VerificationEmail({username, otp: VerifyCode}),
+          });
+        return{ success:true, message: 'Email  send successfully'}
+
+    }catch(emailError){
+        console.error("Error sending Verification Email", emailError)
+        return{ success:false, message: 'Failed to send Verification Email'}
+
+    }
+}
+
