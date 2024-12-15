fa8693669ad46a492405abdd76a75ef1d8485779
Dimple Rana <dimpleranaji@gmail.com>
2024-12-15 13:58:56 +0530

    add auth route

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -40,7 +40,7 @@ export async function POST(request: Request) {
         existingUserByEmail.password = hashedPassword;
         existingUserByEmail.verifyCode = verifyCode;
         existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000);
-        await existingUserByEmail.save()
+        await existingUserByEmail.save();
       }
     } else {
       const hashedPassword = await bcrypt.hash(password, 10);
