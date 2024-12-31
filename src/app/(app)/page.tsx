3cfada56434b693ec268fcf3257de1f1bed2f841
Dimple Rana <dimpleranaji@gmail.com>
2024-12-31 12:18:30 +0530

    Add homepage

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,7 +1,45 @@
-import React from 'react'
+"use client";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import Autoplay from "embla-carousel-autoplay";
+import messages from "@/messages.json";
 
 export default function page() {
   return (
-    <div>page</div>
-  )
+    <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12">
+      <section className="text-center mb-8 md:mb-12">
+        <h1 className="text-3xl md:text-5xl font-bold">
+          Dive into the world of Anonymous conversations
+        </h1>
+        <p></p>
+      </section>
+      <Carousel
+        plugins={[Autoplay({ delay: 2000 })]}
+        className="w-full max-w-xs"
+      >
+        <CarouselContent>
+          {messages.map((message, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1">
+                <Card>
+                  <CardHeader>{message.title}</CardHeader>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </main>
+  );
 }
